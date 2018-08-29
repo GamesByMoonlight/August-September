61f33d69c89230ae611bb69327e158c4b111e676
cisco <ciscom1971@yahoo.com>
2018-08-28 22:17:50 -0500

    Add DragonType Enum

--- August-September/Assets/Scripts/UICounter.cs
+++ August-September/Assets/Scripts/UICounter.cs
@@ -6,22 +6,26 @@ public class UICounter : MonoBehaviour {
     public Text TimerText;
     private float startTime;
     private bool Finished = false;
-	void Start () {
+    void Start() {
         startTime = Time.time;
-	}	
-	// Update is called once per frame
-	void Update () {
-        if(Finished)
+    }
+    // Update is called once per frame
+    void Update() {
+        if (Finished)
         { return; }
         float t = Time.time - startTime;// time in seconds
         string minuites = ((int)t / 60).ToString();
-        string seconds = ((t % 60)*50).ToString("f0"); // f2 for two decimals
-        TimerText.text =  seconds;
-	}
+        string seconds = ((t % 60) * 50).ToString("f0"); // f2 for two decimals
+        TimerText.text = seconds;
+    }
     public void Finish()
     {
         TimerText.color = Color.yellow;
         Finished = true;
+
+
     }
+
+
 
 }
